df0c0c90d09d3dd4b83041f58d80594cab598e01
dangbach2810 <locdangbach@gmail.com>
2024-04-30 09:05:52 +0700

    init pro

---
+++ WebProjectManager.Common/ViewModel/TabViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebProjectManager.Common.ViewModel
+{
+    public class TabViewModel
+    {
+        public string Name { get; set; }
+        public DateTime TimeExpiry { get; set; }
+        public bool IsActive { get; set; }
+        public int Order { get; set; }
+    }
+}
